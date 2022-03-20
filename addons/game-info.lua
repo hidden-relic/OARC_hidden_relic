4c058e82cbe88d0086420d217b985a03dfc46462
hidden-relic <78228823+hidden-relic@users.noreply.github.com>
2022-03-20 07:02:00 -0400

    stdlib update

--- addons/game-info.lua
+++ addons/game-info.lua
@@ -19,19 +19,19 @@ local Styles = require('util/Styles')
 -- ======================================================= --
 local SECTION_CONTENT = {
     {
-        title = '',
+        title = WELCOME_MSG,
         content = {
-            {"", {"Game_Info.info1"}},
-            {"", {"Game_Info.info2"}},
-            {"", {"Game_Info.info3"}}
+            SERVER_MSG,
+            SCENARIO_INFO_MSG,
+            CONTACT_MSG
         }
     }, {
         title = "Train Guidelines",
         content = {
             '*> Trains are [color=orange]RHD[/color] (Right Hand Drive)',
             '*> Please do not make train roundabouts/loops, junctions/end point stations only',
-            '*> General Train size is [color=orange]2-4[/color]',
-            '*> Place junctions 2-4 width apart',
+            '*> General Train size is [color=orange]1-4[/color]',
+            '*> Place junctions 1-4 width apart',
             '*> [color=orange]Color[/color] the trains/stations appropriately'
         }
     }, {
