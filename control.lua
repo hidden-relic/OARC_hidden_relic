8990e2fb3cf5e6e4a5e14c732ae28ce5e0707da8
oof2win2 <honza.koco44@gmail.com>
2022-09-14 21:44:18 +0200

    fix(fagc): change variables to local

--- control.lua
+++ control.lua
@@ -140,12 +140,12 @@ script.on_init(function(event)
 end)
 
 script.on_event(defines.events.on_player_banned, function (e)
-    text = "ban;\"" .. e.player_name .. "\";\"" .. (e.by_player or "") .. "\";\"" .. (e.reason or "") .. "\""
+    local text = "ban;\"" .. e.player_name .. "\";\"" .. (e.by_player or "") .. "\";\"" .. (e.reason or "") .. "\""
     game.write_file("fagc-actions.txt", text, true, 0)
 end)
 
 script.on_event(defines.events.on_player_unbanned, function (e)
-    text = "unban;\"" .. e.player_name .. "\";\"" .. (e.by_player or "") .. "\";\"" .. (e.reason or "") .. "\""
+    local text = "unban;\"" .. e.player_name .. "\";\"" .. (e.by_player or "") .. "\";\"" .. (e.reason or "") .. "\""
     game.write_file("fagc-actions.txt", text, true, 0)
 end)
 
