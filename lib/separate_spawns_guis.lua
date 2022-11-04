4a8014288b40d34014d9e7b373ae63a510affed0
hidden-relic <the.true.hidden.relic@gmail.com>
2022-11-04 09:51:02 -0500

    teams?

--- lib/separate_spawns_guis.lua
+++ lib/separate_spawns_guis.lua
@@ -764,14 +764,14 @@ function SpawnCtrlGuiClick(event)
 
                 -- Spawn the player
                 local joiningPlayer = game.players[joinQueuePlayerChoice]
-                ChangePlayerSpawn(joiningPlayer, global.ocore.sharedSpawns[player.name].position)
-                SendPlayerToSpawn(joiningPlayer)
-                GivePlayerStarterItems(joiningPlayer)
+                local newSpawn = FindUngeneratedCoordinates(global.ocfg.near_dist_start,global.ocfg.far_dist_end, joiningPlayer.surface)
+                ChangePlayerSpawn(joiningPlayer, newSpawn)
+                QueuePlayerForDelayedSpawn(joiningPlayer.name, newSpawn, moatChoice, global.ocfg.enable_vanilla_spawns)
                 table.insert(global.ocore.sharedSpawns[player.name].players, joiningPlayer.name)
                 joiningPlayer.force = game.players[player.name].force
 
-                -- Render some welcoming text...
-                DisplayWelcomeGroundTextAtSpawn(joiningPlayer, global.ocore.sharedSpawns[player.name].position)
+                -- -- Render some welcoming text...
+                -- DisplayWelcomeGroundTextAtSpawn(joiningPlayer, global.ocore.sharedSpawns[player.name].position)
 
                 -- Unlock spawn control gui tab
                 SetOarcGuiTabEnabled(joiningPlayer, OARC_SPAWN_CTRL_GUI_NAME, true)
